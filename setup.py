7a6738f0a9241e622b092d2aacc8b4dc60aa96cd
Shawn Crosby <scrosby@salesforce.com>
2013-07-27 08:31:40 -0300

    updated packaging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='r6_commit',
-    version='0.1.14',
+    version='0.1.15',
     author='Shawn Crosby',
     author_email='scrosby@salesforce.com',
     packages=find_packages(),
