f1139729331c6eb03e13bcf1826a20eee0433d6a
Shawn Crosby <scrosby@salesforce.com>
2013-09-05 13:00:56 -0300

    Refactored hooks to a separate project GitParse is now just a git log parser.

--- setup.py
+++ setup.py
@@ -1,20 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='r6_commit',
-    version='0.1.22',
+    name='git_logparser',
+    version='0.1.0',
     author='Shawn Crosby',
     author_email='scrosby@salesforce.com',
     packages=find_packages(),
     license='Be Real',
     url='https://pypi.python.org/pypi?r6_commit',
-    description='Git Post Commit hook to parse log and create code review',
+    description='Library to parse Git Logs',
     long_description=open('README.txt').read(),
-    scripts=['bin/setup_git_hooks.py', 'bin/post-commit', 'bin/commit-msg'],
-    install_requires=[
-        "gus_client>=0.1.11",
-        "ccollab_client>=0.1.8",
-        "jenkinsapi",
-        "sc_pylibs>=0.1.1",
-    ],
 )
